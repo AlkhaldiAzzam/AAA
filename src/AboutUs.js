8d959661ce73c21e7c3b844120bf4324577c2843
ASSROUGUI <ASSROUGUI@hotmail.com>
2019-08-20 11:15:57 +0300

    ...

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,6 +1,8 @@
 
 import React ,{Component} from  'react';
 import './AboutUs.css';
+import {CardDeck, Card, CardGroup, Col} from 'react-bootstrap';
+
 class AboutUs extends Component{
 render(){
 
@@ -8,8 +10,13 @@ render(){
      
 
 <div>
-
-   <p>AboutUs</p>
+<link
+  rel="stylesheet"
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+  crossorigin="anonymous"
+/>
+   <div class='AboutUs'>AboutUs</div>
 
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
@@ -17,43 +24,63 @@ render(){
 
 
 
-<div class="card">
-  <img src="img.jpg" alt="Ashwag" />
-  <h1>Azzam</h1>
-  <p class="title">....cs</p>
-  <p>... University</p>
- <a href="#"><i class="fa fa-github"></i></a> 
-  <a href="#"><i class="fa fa-twitter"></i></a> 
-  <a href="#"><i class="fa fa-linkedin"></i></a> 
 
-  <p><div class="black">Contact</div></p>
-</div> 
+<CardDeck>
+<Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Azzam Alkhaldi</Card.Title>
+      <Card.Text>
+    ...
+  {/* <p><div class="black">Contact</div></p> */}
 
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <a href="https://github.com/AlkhaldiAzzam/aaa"><i class="fa fa-github"style={{ backgroundColor: 'white', color: 'black' }}></i></a> {' '}  
+  <a href="#"><i class="fa fa-envelope"style={{ backgroundColor: 'white', color: '#A4C0E4' }}></i></a>  {' '}
+  <a href="#"><i class="fa fa-phone"style={{ backgroundColor: 'white', color: 'green' }}></i></a> {' '}
+  <a href="#"><i class="fa fa-linkedin"></i></a>  {' '}
+  <p><div class="black">Contact</div></p> 
 
-<div class="card">
-  <img src="img.jpg" alt="Ashwag" />
-  <h1>Ashwag Alrougui</h1>
-  <p class="title">....cs</p>
-  <p>... University</p>
- <a href="#"><i class="fa fa-github"></i></a> 
-  <a href="#"><i class="fa fa-twitter"></i></a> 
-  <a href="#"><i class="fa fa-linkedin"></i></a> 
+     </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Ashwag Alrougui</Card.Title>
+      <Card.Text>
+   ...
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer> 
+    <a href="https://github.com/ASSROUGUI/aaa"><i class="fa fa-github"style={{ backgroundColor: 'white', color: 'black' }}></i></a> {' '}  
+  <a href="#"><i class="fa fa-envelope"style={{ backgroundColor: 'white', color: '#A4C0E4' }}></i></a>  {' '}
+  <a href="#"><i class="fa fa-phone"style={{ backgroundColor: 'white', color: 'green' }}></i></a> {' '}
+  <a href="#"><i class="fa fa-linkedin"></i></a>  {' '}
+    <p><div class="black">Contact</div></p> 
+  
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Abdullah Aaltwaim</Card.Title>
+      <Card.Text>
+    ....
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <a href="https://github.com/aaltwaim/aaa/tree/abdullah"><i class="fa fa-github"style={{ backgroundColor: 'white', color: 'black' }}></i></a> {' '}  
+  <a href="#"><i class="fa fa-envelope"style={{ backgroundColor: 'white', color: '#A4C0E4' }}></i></a>  {' '}
+  <a href="#"><i class="fa fa-phone"style={{ backgroundColor: 'white', color: 'green' }}></i></a> {' '}
+  <a href="#"><i class="fa fa-linkedin"></i></a>  {' '}
+    <p><div class="black">Contact</div></p> 
+    </Card.Footer>
+  </Card>
+</CardDeck>
+      
 
-  <p><div class="black">Contact</div></p>
-</div> 
-
-
-<div class="card">
-  <img src="img.jpg" alt="Ashwag" />
-  <h1>Abdullah</h1>
-  <p class="title">....cs</p>
-  <p>... University</p>
- <a href="#"><i class="fa fa-github"></i></a> 
-  <a href="#"><i class="fa fa-twitter"></i></a> 
-  <a href="#"><i class="fa fa-linkedin"></i></a> 
-
-  <p><div class="black">Contact</div></p>
-</div> 
 
 </div>
 
--- src/Drama.js
+++ src/Drama.js
@@ -1,14 +1,67 @@
 import React ,{Component} from  'react';
+// import CardGroup from 'react-bootstrap/CardGroup'
+// import Card from 'react-bootstrap/Card'
+// import CardDeck from 'react-bootstrap/CardDeck'
+import {CardDeck, Card, CardGroup, Col} from 'react-bootstrap';
+
 
 class Drama extends Component{
 render(){
-//     const scene =  this.state.scene;
-//     if (  this.state.nextIndex ==''){
-// console.log('xxx');
-//     }
-    return(
-        <p>drama</p>
 
+    return(
+<div>
+<link
+  rel="stylesheet"
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+  crossorigin="anonymous"
+/>
+
+<CardDeck>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+</CardDeck>
+      
+
+</div>
 
        
     )
